d904b5cc9a835df8405ab14ab9d42ffcbe3a723e
AxelJurczuk <61692326+AxelJurczuk@users.noreply.github.com>
2021-02-03 11:53:20 -0300

    code associated with viewModel

---
+++ app/src/main/java/com/example/android/employeesdetails/viewmodel/EmployeeViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.android.employeesdetails.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android.employeesdetails.data.EmployeeDataSet
+import com.example.android.employeesdetails.data.Result
+import com.example.android.employeesdetails.model.Employee
+
+class EmployeeViewModel : ViewModel() {
+
+
+    val liveDataList = MutableLiveData<Result>()
+    private val employeeDataSet = EmployeeDataSet()
+
+    init {
+        getEmployeeList()
+    }
+
+    private fun getEmployeeList (){
+        employeeDataSet.getEmployeeList(object : EmployeeDataSet.OnResultCallback {
+            override fun onResult(result: Result) {
+                if (result is Result.Success){
+                    liveDataList.value= result
+                }
+            }
+        })
+    }
+    fun getListData (): LiveData<Result>{
+        return liveDataList
+    }
+
+}
