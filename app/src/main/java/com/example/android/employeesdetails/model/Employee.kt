bcc703a23a5107e6792952a3cbdf1785b81fb6a3
AxelJurczuk <61692326+AxelJurczuk@users.noreply.github.com>
2021-02-03 08:41:06 -0300

    code associated with data

--- app/src/main/java/com/example/android/employeesdetails/model/Employee.kt
+++ app/src/main/java/com/example/android/employeesdetails/model/Employee.kt
@@ -1,10 +1,11 @@
 package com.example.android.employeesdetails.model
 
 import android.os.Parcelable
+import com.squareup.moshi.Json
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Employee (val name:String, val email:String, val geo: Geo, val website:String):Parcelable
 
 @Parcelize
-data class Geo (val lat:Double, val lng:Double): Parcelable
+data class Geo (val lat:Double, @Json(name="lng")val long:Double): Parcelable
